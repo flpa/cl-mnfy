ff370af74c8a84ea1aa7f9d8ab2c264afe322903
flpa <flpa.dev@zoho.com>
2015-07-20 22:11:52 +0200

    prototype for minifying function names

--- cl-mnfy.lisp
+++ cl-mnfy.lisp
@@ -7,12 +7,43 @@
     for sym in code
     do (if (listp sym)
          (print-code sym)
-         (format t "~:[~; ~]~:s" (and (not (listp prev))
-                                      (not (listp sym))) sym))
+         ;; print symbols with ~a to get 'ab' instead of '#:ab'
+         ;; rest with ~s to get escape chars (e.g. '"hello"')
+         (format t "~:[~; ~]~:[~s~;~a~]" (and (not (listp prev))
+                                      (not (listp sym))) (symbolp sym) (mini sym)))
     finally (format t ")"))) 
 
+(defun mini (sym)
+  (if (minifiable-p sym)
+    ;; must check if used: fboundp, or as symbol?
+    (make-symbol (subseq (symbol-name sym) 0 1))
+    sym
+    ))
+
 (defun minify (filename)
+  (setf *shortened-symbols* (make-hash-table :test #'equal))
   (print-code (with-open-file (in filename)
                 (read in))))
 
-;;(minify "res/test.lisp")
+(minify "~/code/lisp/cl-mnfy/res/test.lisp")
+
+(defun minifiable-p (x)
+  (and x
+       (symbolp x)
+       (> (length (symbol-name x)) 1)
+       (not (or (fboundp x)
+                (eql t x)))))
+
+(defparameter *shortened-symbols* nil)
+
+;;TODO: unit test
+(assert (equal '(abc) 
+               (remove-if-not 
+                 #'minifiable-p 
+                 '(defun abc () 
+                    (princ 1)
+                    #\c
+                    10
+                    nil
+                    t
+                    ))))
